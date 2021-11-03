aaf787cf9e5205a141c9297629b861a4f074905a
Trevor Green <git@trumpi.co.za>
2021-11-03 00:15:04 -0400

    Recognise proxy headers.

--- src/Drako.Api/Startup.cs
+++ src/Drako.Api/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +30,10 @@ namespace Drako.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.All;
+            });
             services.AddControllers();
             services.AddSignalR();
             services.AddSwaggerGen(c =>
@@ -97,6 +102,8 @@ namespace Drako.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders();
+            
             var pathBase = Configuration.GetSection("http")["pathBase"];
             if (!string.IsNullOrWhiteSpace(pathBase))
             {
