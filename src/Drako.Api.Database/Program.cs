2ab6f91916b786ffa56eb05fd45b2c872469d770
Trevor Green <git@trumpi.co.za>
2021-12-11 20:22:51 -0500

    Move data migrations to docker image.

---
+++ src/Drako.Api.Database/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using DbUp;
+using Microsoft.Extensions.Configuration;
+
+namespace Drako.Api.Database
+{
+    class Program
+    {
+        static int Main()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json", true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = configuration["database:connectionString"];
+
+            var upgradeEngine = DeployChanges.To.PostgresqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .Build();
+
+            var result = upgradeEngine.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
